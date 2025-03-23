d195b75b2bf5b34cfbbba587ec7ac9a9b8e4042c
RocXOvO <1376107905@qq.com>
2025-03-23 15:18:16 +0800

    Fix the error.

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -11,6 +11,7 @@ public class TestArrayDequeEC {
     public void randomTest(){
         StudentArrayDeque<Integer> bugArrayDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> correctArrayDeque = new ArrayDequeSolution<>();
+        String message = "";
         int N = 5000;
         for (int i = 0;i < N;i++){
             int OpNumber = StdRandom.uniform(0,4);
@@ -18,24 +19,24 @@ public class TestArrayDequeEC {
             if (OpNumber == 0) {
                 bugArrayDeque.addFirst(RandomNumber);
                 correctArrayDeque.addFirst(RandomNumber);
-                System.out.println("removeFirst(" + RandomNumber + ")");
+                message += "addFirst(" + RandomNumber + ")\n";
             }else if (OpNumber == 1){
                 bugArrayDeque.addLast(RandomNumber);
                 correctArrayDeque.addLast(RandomNumber);
-                System.out.println("removeLast(" + RandomNumber + ")");
+                message += "addLast(" + RandomNumber + ")\n";
             }else if (OpNumber == 2){
                 if (!bugArrayDeque.isEmpty() && !correctArrayDeque.isEmpty()){
                     Integer a = correctArrayDeque.removeFirst();
                     Integer b = bugArrayDeque.removeFirst();
-                    System.out.println("removeFirst()");
+                    message += "removeFirst()\n";
                     assertEquals("Oh no!This is fucking bad:\n removeFirst is expected to be " + a + ",but turns out " + b + ".", a, b);
                 }
             }else if (OpNumber == 3){
                 if (!bugArrayDeque.isEmpty() && !correctArrayDeque.isEmpty()) {
                     Integer a = correctArrayDeque.removeLast();
                     Integer b = bugArrayDeque.removeLast();
-                    System.out.println("removeLast()");
-                    assertEquals("Oh no!This is fucking bad:\n removeLast is expected to be " + a + ",but turns out " + b + ".", a, b);
+                    message += "removeLast()\n";
+                    assertEquals(message, a, b);
                 }
             }
         }
